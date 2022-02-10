16ee04c86f5e67554aac9cf987208420a02da216
Valentyn <iwilly17@gmail.com>
2022-02-10 14:38:07 +0200

    changed register form

--- FormCRM/RegistrationUser.py
+++ FormCRM/RegistrationUser.py
@@ -31,7 +31,7 @@ def init_registration(bot, message):
     """
 
     bot_value.bot = bot
-    bot_value.bot.send_message(message.chat.id, "Введите свое имя")
+    bot_value.bot.send_message(message.chat.id, "Введите свое имя. (Для выхода введите exit)")
     bot_value.bot.register_next_step_handler(message, register_name)
 
 
@@ -43,16 +43,22 @@ def register_name(message):
     """
     try:
         name = message.text
-        if len(name) and name.isalpha():
-            bot_value.bot.send_message(message.chat.id, "Введите номер телефона")
-            user_dict["name"] = name
-            bot_value.bot.register_next_step_handler(message, register_phone)
+        if name == 'exit' or name == 'Exit':
+
+            from Command import ForStartMenu
+            ForStartMenu.some_action(message, bot_value.bot)
+
         else:
-            bot_value.bot.send_message(
-                message.chat.id, "Ошибка, имя не должно содержать неподходящие символы."
-            )
-            bot_value.bot.send_message(message.chat.id, "Введите имя заново")
-            bot_value.bot.register_next_step_handler(message, register_name)
+            if len(name) and name.isalpha():
+                bot_value.bot.send_message(message.chat.id, "Введите номер телефона. (Для выхода введите exit)")
+                user_dict["name"] = name
+                bot_value.bot.register_next_step_handler(message, register_phone)
+            else:
+                bot_value.bot.send_message(
+                    message.chat.id, "Ошибка, имя не должно содержать неподходящие символы."
+                )
+                bot_value.bot.send_message(message.chat.id, "Введите имя заново")
+                bot_value.bot.register_next_step_handler(message, register_name)
     except Exception:
         bot_value.bot.send_message(message.chat.id, "Ошибка, попробуйте заново")
 
@@ -65,21 +71,27 @@ def register_phone(message):
     """
     try:
         currect_number = message.text
-        if currect_number.isdigit() or currect_number.startswith("+"):
-            user_dict["phone"] = currect_number
-            bot_value.bot.send_message(message.chat.id, "Введите свой email")
-            bot_value.bot.register_next_step_handler(message, register_email)
+        if currect_number == 'exit' or currect_number == 'Exit':
+
+            from Command import ForStartMenu
+            ForStartMenu.some_action(message, bot_value.bot)
+
         else:
-            bot_value.bot.send_message(
-                message.chat.id,
-                "Вы неправильно ввели номер телефона (номер не должен содержать букв) попробуйте ещё раз",
-            )
-            bot_value.bot.send_message(message.chat.id, "Введите свой номер телефона")
-            bot_value.bot.register_next_step_handler(message, register_phone)
+            if currect_number.isdigit() or currect_number.startswith("+"):
+                user_dict["phone"] = currect_number
+                bot_value.bot.send_message(message.chat.id, "Введите свой email. (Для выхода введите exit)")
+                bot_value.bot.register_next_step_handler(message, register_email)
+            else:
+                bot_value.bot.send_message(
+                    message.chat.id,
+                    "Вы неправильно ввели номер телефона (номер не должен содержать букв) попробуйте ещё раз",
+                )
+                bot_value.bot.send_message(message.chat.id, "Введите свой номер телефона.")
+                bot_value.bot.register_next_step_handler(message, register_phone)
     except ValueError:
         bot_value.bot.send_message(
             message.chat.id,
-            "Вы неправильно ввели номер телефона (номер не должен содержать букв, попробуйте ещё раз",
+            "Вы неправильно ввели номер телефона (номер не должен содержать букв).  Попробуйте ещё раз",
         )
         register_phone(message)
 
@@ -93,18 +105,22 @@ def register_email(message):
     regex = re.compile(
         r"([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]+(\.[A-Z|a-z]{2,})+"
     )
+    if message.text == 'exit' or message.text == 'Exit':
 
-    if re.fullmatch(regex, message.text):
-        user_dict["email"] = message.text
-        bot_value.bot.register_next_step_handler(message, final_register)
-        bot_value.bot.send_message(message.chat.id, "Введите краткое описание")
+        from Command import ForStartMenu
+        ForStartMenu.some_action(message, bot_value.bot)
     else:
-        bot_value.bot.send_message(
-            message.chat.id, "Ошибка, email не соответствует формату, попробуйте заново"
-        )
-        bot_value.bot.send_message(message.chat.id, "Введите свой email")
+        if re.fullmatch(regex, message.text):
+            user_dict["email"] = message.text
+            bot_value.bot.register_next_step_handler(message, final_register)
+            bot_value.bot.send_message(message.chat.id, "Введите краткое описание. (Для выхода введите exit)")
+        else:
+            bot_value.bot.send_message(
+                message.chat.id, "Ошибка, email не соответствует формату, попробуйте заново"
+            )
+            bot_value.bot.send_message(message.chat.id, "Введите свой email")
 
-        bot_value.bot.register_next_step_handler(message, register_email)
+            bot_value.bot.register_next_step_handler(message, register_email)
 
 
 def final_register(message):
@@ -113,15 +129,20 @@ def final_register(message):
     :param message: information about chat
     :return: None
     """
-    bot_value.bot.send_message(
-        message.chat.id,
-        "Вы успешно зарегестрировались. С Вами свяжется наш администратор",
-    )
-    user_dict["info"] = message.text
-    user_dict["data"] = date.today().strftime("%d/%m/%Y")
-    sendletter.send_mail(user_dict)
-    bot_value.bot.send_message(
-        469236353,
-        f"Заявка від:\n{user_dict['name']}\n{user_dict['email']}\n+{user_dict['phone']}\n\n" \
-        f"Текст повідомлення:\n{user_dict['info']}\nДата\n{user_dict['data']}")
-    return user_dict
+    if message.text == 'exit' or message.text == 'Exit':
+
+        from Command import ForStartMenu
+        ForStartMenu.some_action(message, bot_value.bot)
+    else:
+        bot_value.bot.send_message(
+            message.chat.id,
+            "Вы успешно зарегестрировались. С Вами свяжется наш администратор",
+        )
+        user_dict["info"] = message.text
+        user_dict["data"] = date.today().strftime("%d/%m/%Y")
+        sendletter.send_mail(user_dict)
+        bot_value.bot.send_message(
+            469236353,
+            f"Заявка від:\n{user_dict['name']}\n{user_dict['email']}\n+{user_dict['phone']}\n\n" \
+            f"Текст повідомлення:\n{user_dict['info']}\nДата\n{user_dict['data']}")
+        return user_dict
