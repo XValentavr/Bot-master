777fe5c81db4f96a5f012172ed9618499029300e
Valentyn <iwilly17@gmail.com>
2022-02-11 12:44:04 +0200

    changed len of button

--- RegexMethods/delete_if_more_than_64.py
+++ RegexMethods/delete_if_more_than_64.py
@@ -9,12 +9,12 @@ def del_if_64(text, counter):
     final_global_counties = [None for _ in range(len(text))]
     for i in text:
         new_county = str(i)
-        if new_county.endswith('(Харківське'):
-            new_county.replace('(Харківське', '')
+        if '(Харківське військове поселення)' in new_county:
+            new_county = new_county.replace('(Харківське військове поселення)', '')
         while (len(new_county.encode("utf-8"))) > 64:
             new_county = new_county.split()
             new_county.pop()
             new_county = " ".join(map(str, new_county))
         final_global_counties[index] = new_county.strip()
         index -= 1
-    return final_global_counties
+    return list(set(final_global_counties))
