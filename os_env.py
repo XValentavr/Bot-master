1083483489af5b91eda5188d0c69aad80c67d7a4
xvalentavr <iwilly17@gmail.com>
2022-10-01 16:36:37 +0300

    changed  logic of showing messages to user

--- os_env.py
+++ os_env.py
@@ -3,6 +3,6 @@ import os
 os.environ["url"] = "https://genealogia.salesdrive.me/handler/"
 
 os.environ["form"] = "WYqamAuW8x9lO7e2iFEcIWDGAvxmUX2BrP-HtP4NAzgSOmAnK79n"
-os.environ['TOKEN'] = "5650798100:AAFJAdEl3CuEn0mezPPJxXp_sun8nCIrGbk"
+os.environ['TOKEN'] = "1362750182:AAF8LlEm790xbapCImuE5Bd77LXp6WdEeuw"
 
 #5650798100:AAFJAdEl3CuEn0mezPPJxXp_sun8nCIrGbk
